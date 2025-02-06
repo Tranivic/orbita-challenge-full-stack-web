d4cc91f0b02ff596effd80dbf7986ca690f4a9eb
Victor Trani <victor@wdhouse.com.br>
2025-02-06 18:23:49 -0300

    feat(backend):add limiter for students route

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,12 +1,19 @@
 import express from "express";
 import multer from 'multer';
 const upload = multer();
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 80,
+    message: "Muitas tentativas, tente novamente mais tarde"
+});
 
 import { studentController } from "../controllers/student.js";
 
 const router = express.Router();
 
-router.get("/students", studentController.listStudents);
+router.get("/students", limiter, studentController.listStudents);
 router.post("/students", upload.none(), studentController.createStudent);
 // router.put("/students/:id",  upload.none(), studentController.updateStudent);
 // router.delete("/students/:id", studentController.deleteStudent);
