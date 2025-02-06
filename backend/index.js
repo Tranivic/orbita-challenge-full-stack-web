6032877679c4256a7d38556d3f02e7c4a967ea82
Victor Trani <victor@wdhouse.com.br>
2025-02-05 22:02:41 -0300

    feat(backend): add initial structure

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from './src/routes/routes.js';
+import { connect } from './src/prisma.js';
+import cors from 'cors';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, async () => {
+    try {
+        await connect();
+        console.log("Server running on port: " + port);
+    } catch (error) {
+        console.error('Error on server start:', error);
+    }
+});
